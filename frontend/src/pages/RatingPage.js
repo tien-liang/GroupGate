77291bc24b9d9ad9f0b5c36f757c299a0d5be7c6
Jason <jsoliman@sfu.ca>
2018-03-28 16:15:53 -0700

    Update sign in/rating pages for clearer text/proper rating numbers

--- frontend/src/pages/RatingPage.js
+++ frontend/src/pages/RatingPage.js
@@ -10,11 +10,6 @@ const options = [
   {key:'3', text: '3', value: 3},
   {key:'4', text: '4', value: 4},
   {key:'5', text: '5', value: 5},
-  {key:'6', text: '6', value: 6},
-  {key:'7', text: '7', value: 7},
-  {key:'8', text: '8', value: 8},
-  {key:'9', text: '9', value: 9},
-  {key:'10', text: '10', value: 10}
 ]
 
 export default class Rating extends Component {
@@ -31,7 +26,7 @@ export default class Rating extends Component {
           <div className="ui segment">
           <Grid centered columns={3}>
             <Grid.Column>
-              <h2>Rating</h2>
+              <h2>Group Member Rating</h2>
               <h6>You are currently rating for <strong>Jessica C.</strong></h6>
               <br/>
           <Form className="ui form center">
@@ -40,7 +35,7 @@ export default class Rating extends Component {
             <Form.Field control={Select} label='Communication' options={options} placeholder='-' width={6} />
             <Form.Field control={Select} label='Time Management' options={options} placeholder='-' width={6} />
             <Form.Field control={Select} label='Activity' options={options} placeholder='-' width={6} />
-            <Form.Field control={TextArea} label='Comment' placeholder='Add more comment...' />
+            <Form.Field control={TextArea} label='Comment' placeholder='Add more comments...' />
             <Form.Field control={Button}>Submit</Form.Field>
           </Form>
         </Grid.Column>
