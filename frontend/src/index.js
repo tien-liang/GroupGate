ab202f2daeb133982c0d6a0cb64ae043c90da150
Davorin Doung <dakesiau@gmail.com>
2018-03-28 19:57:34 -0700

    Add auth token handling

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from "react-router-dom";
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'semantic-ui-css/semantic.min.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import createHistory from 'history/createBrowserHistory'
+import { ConnectedRouter } from 'react-router-redux'
+import { Provider } from 'react-redux'
+import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import App from './App';
+import configureStore from './store'
 
-ReactDOM.render(
-	<BrowserRouter>
-		<App />
-	</BrowserRouter>,
-	document.getElementById('root'));
+const history = createHistory()
+const store = configureStore(history)
+ReactDOM.render((
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <App />
+    </ConnectedRouter>
+  </Provider>
+), document.getElementById('root'));
 
 registerServiceWorker();
